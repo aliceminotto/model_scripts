4261cb494cd11eaf44e86ff846c10bc2442902e8
aliceminotto <i.m.e@hotmail.it>
2015-10-23 14:32:15 +0100

    new launch file

--- simulation/LNKMODEL/launch.sh
+++ simulation/LNKMODEL/launch.sh
@@ -1,2 +1,2 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/tsl/software/testing/gsl/1.16/x86_64/bin/lib
-bsub -q TSL-Prod128 -We 1 "source python-2.7.4; python /usr/users/sl/lugoc/PLLMODEL1/MODEL1/RUNSL2/LNKMODEL/mprostest.py"
+bsub -q TSL-Prod128 -We 1 "source python-2.7.4; python /usr/users/TSL_20/minottoa/modifiche/LNKMODEL/mprostest.py"
