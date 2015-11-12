c0adfbf7e1a3f456dd7ea70a3076ada18a14f138
aliceminotto <i.m.e@hotmail.it>
2015-11-12 13:58:56 +0000

    modified to add new file

--- plotting/launch_comparison_plot.sh
+++ plotting/launch_comparison_plot.sh
@@ -1,2 +1,2 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/tsl/software/testing/gsl/1.16/x86_64/bin/lib; \
-bsub -q TSL-Prod128 -We 1 "source python-2.7.4; python /usr/users/TSL_20/minottoa/model_scripts/plotting/comparison_plot.py ~/change_DT_Qi/DT5000/ok_20/ ~/change_DT_Qi/DT10000/ok_20/ ~/change_DT_Qi/DT15000/ok_20/ ~/change_DT_Qi/DT20000/ok_20/ 20"
+bsub -q TSL-Prod128 -We 1 "source python-2.7.4; python /usr/users/TSL_20/minottoa/model_scripts/plotting/comparison_plot.py ~/change_DT_Qi/DT5000/ok_20/ ~/change_DT_Qi/DT10000/ok_20/ ~/change_DT_Qi/DT15000/ok_20/ ~/change_DT_Qi/DT20000/ok_20/ ~/new/"
